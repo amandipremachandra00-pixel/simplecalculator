7133886a6435a4b53f21c119262a26f7ba7a7e19
shamudi <shamudiwanasinghe@gmail.com>
2026-05-25 09:17:44 +0530

    init

---
+++ simplecalculations.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    printf("addition:",2+3);
+    printf("\nsubtraction:",10-3);
+    a=3;
+    printf("\n%d",a);
+    printf("\n%d",90/3);
+    b=a+b;
+    printf("\n%d",b);
+    return 0;
+
+}
